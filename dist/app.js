87e7e47381b19418042d86199744166e50bb0944
youngseoKim-kr <ysk5754@naver.com>
2022-07-07 21:57:27 +0900

    Add : video.ts

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 import { PageComponent } from "./components/page/page.js";
 class App {
     constructor(appRoot) {
@@ -8,6 +9,8 @@ class App {
         this.page.attachTo(appRoot);
         const image = new ImageComponent("Image Title", "https://picsum.photos/600/300");
         image.attachTo(appRoot, "beforeend");
+        const video = new VideoComponent("Video Title", "https://youtu.be/K3-jG52XwuQ");
+        video.attachTo(appRoot, "beforeend");
         const note = new NoteComponent("Note Title", "Note Body");
         note.attachTo(appRoot, "beforeend");
         const todo = new TodoComponent("Todo Title", "Todo Item");
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
+import { VideoComponent } from "./components/page/item/video.js";
 import { PageComponent } from "./components/page/page.js";
 
 class App {
@@ -14,6 +15,12 @@ class App {
       "https://picsum.photos/600/300"
     );
     image.attachTo(appRoot, "beforeend");
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://youtu.be/K3-jG52XwuQ"
+    );
+    video.attachTo(appRoot, "beforeend");
 
     const note = new NoteComponent("Note Title", "Note Body");
     note.attachTo(appRoot, "beforeend");
